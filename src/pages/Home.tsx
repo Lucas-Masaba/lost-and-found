b9f055d4ad4b7b91168c6835cfdcce56d452b07a
Lucas-Masaba <masabaluke@gmail.com>
2023-09-20 16:38:43 +0300

    Configure tailwind

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,9 @@
+import '../../src/assets/test.css'
+
 const Home = () => {
   return (
     <div className="Home">
-      <h1>Home</h1>
+      <h1 className="text-3xl font-bold underline ">Home</h1>
     </div>
   );
 }
