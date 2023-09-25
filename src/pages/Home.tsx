3e3d8a4004cb8152f3117e5450384e8353611386
Lucas-Masaba <masabaluke@gmail.com>
2023-09-25 18:17:07 +0300

    Add Navbar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,9 @@
-import '../../src/assets/test.css'
+import Navbar from "../components/Navabar";
 
 const Home = () => {
   return (
     <div className="Home">
+      <Navbar />
       <h1 className="text-3xl font-bold underline ">Home</h1>
     </div>
   );
